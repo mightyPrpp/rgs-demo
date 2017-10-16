fe43f9c7fd3f68cb67892df24a0ea9181278f80f
Anna Ange <anna.ange@hotmail.com>
2017-10-16 13:40:58 +0300

    securityconfig

--- src/main/java/com/example/demoproject/SpringSecurityConfig.java
+++ src/main/java/com/example/demoproject/SpringSecurityConfig.java
@@ -19,10 +19,26 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/login"))
-                .and().formLogin().defaultSuccessUrl("/")
-                .loginPage("/login").and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+       // http.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/login"))
+              //  .and().formLogin().defaultSuccessUrl("/")
+              //  .loginPage("/login").and()
+               // .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+
+
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().fullyAuthenticated()
+                .and().csrf().disable()
+                .formLogin().successHandler(successHandler)
+                .loginPage("/login")
+                .permitAll()
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll();
     }
 
     @Autowired

19381abd79e22f2ded39e2b9c762653da81b63ad
prpp <nick.androul@gmail.com>
2017-10-17 19:59:48 +0300

    there I fixed it

--- src/main/java/com/example/demoproject/Repositories/UserRepository.java
+++ src/main/java/com/example/demoproject/Repositories/UserRepository.java
@@ -1,4 +1,17 @@
 package com.example.demoproject.Repositories;
 
-public interface UserRepository {
+import com.example.demoproject.Domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Page<User> findAll(Pageable pageable);
+
+    User findByUsernameAndPassword(String username, String password);
+
+    User save(User user);
+
 }
